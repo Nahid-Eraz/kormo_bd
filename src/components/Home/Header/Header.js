a14f2d675bd330af9824d259d81469d6e3fac586
Nahid-Eraz <nahidulislameraz@gmail.com>
2021-08-19 00:50:37 +0600

    header done

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import NavBar from '../NavBar/NavBar';
+import './Header.css';
 
 
 const Header = () => {
     return (
-        <div>
+        <main className="header-container">
             <NavBar></NavBar>
             <Banner></Banner>
-        </div>
+        </main>
     );
 };
 
