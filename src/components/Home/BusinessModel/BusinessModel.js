8f08ef5f386e344ab8e06f841b2765b332ca2ddb
Nahid-Eraz <nahidulislameraz@gmail.com>
2021-08-19 14:35:47 +0600

    business model carosel done

--- src/components/Home/BusinessModel/BusinessModel.js
+++ src/components/Home/BusinessModel/BusinessModel.js
@@ -1,44 +1,81 @@
 import React from 'react';
-import { Carousel, Container } from 'react-bootstrap';
+import { Carousel, Col, Container, Image, Row } from 'react-bootstrap';
+import Fade from 'react-reveal/Fade';
+import LightSpeed from 'react-reveal/LightSpeed';
+import CustomerService from '../../../images/247.jpg';
+import RapidResponse from '../../../images/rapid.jpg';
+import Worker from '../../../images/worker1.jpg';
 
 const BusinessModel = () => {
     return (
-        <Container>
-            <Carousel variant="dark">
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="holder.js/800x400?text=First slide&bg=f5f5f5"
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h5>24/7 Service</h5>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="holder.js/800x400?text=Second slide&bg=eee"
-                        alt="Second slide"
-                    />
-                    <Carousel.Caption>
-                        <h5>Rapid Response</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-                        alt="Third slide"
-                    />
-                    <Carousel.Caption>
-                        <h5>Vast Crews</h5>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+        <Container fluid className="mt-5">
+
+            <article>
+                <Fade top>
+                    <div className="text-center">
+                        <span style={{ color: "#e7388f" }}>Our Speciality</span>
+                        <h2>Why You Choose Us</h2>
+                    </div>
+                </Fade>
+            </article>
+
+            <article>
+                <Carousel variant="dark">
+                    <Carousel.Item>
+                        <LightSpeed>
+                            <Row className="align-items-center justify-content-center ">
+                                <Col md={4} className="p-md-5 order-2 order-md-1">
+                                    <Fade top duration={2500} distance="40px">
+                                        <h1 style={{ color: "#e7388f" }}>24/7 <br /> Services</h1>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe architecto voluptatum harum at officia totam veniam earum accusantium quibusdam. Repudiandae.</p>
+                                    </Fade>
+                                </Col>
+                                <Col md={6} className="p-md-5 order-1 order-md-2">
+                                    <Fade bottom duration={2500} distance="40px">
+                                        <Image src={CustomerService} fluid />
+                                    </Fade>
+                                </Col>
+                            </Row>
+                        </LightSpeed>
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                        <LightSpeed>
+                            <Row className="align-items-center justify-content-center ">
+                                <Col md={4} className="p-md-5 order-2 order-md-1">
+                                    <Fade top duration={2500} distance="40px">
+                                        <h1 style={{ color: "#e7388f" }}>Rapid <br /> Response</h1>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe architecto voluptatum harum at officia totam veniam earum accusantium quibusdam. Repudiandae.</p>
+                                    </Fade>
+                                </Col>
+                                <Col md={6} className="p-md-5 order-1 order-md-2">
+                                    <Fade bottom duration={2500} distance="40px">
+                                        <Image src={RapidResponse} fluid />
+                                    </Fade>
+                                </Col>
+                            </Row>
+                        </LightSpeed>
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                        <LightSpeed>
+                            <Row className="align-items-center justify-content-center ">
+                                <Col md={4} className="p-md-5 order-2 order-md-1">
+                                    <Fade top duration={2500} distance="40px">
+                                        <h1 style={{ color: "#e7388f" }}>Vast <br /> Worker</h1>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe architecto voluptatum harum at officia totam veniam earum accusantium quibusdam. Repudiandae.</p>
+                                    </Fade>
+                                </Col>
+                                <Col md={6} className="p-md-5 order-1 order-md-2">
+                                    <Fade bottom duration={2500} distance="40px">
+                                        <Image src={Worker} fluid />
+                                    </Fade>
+                                </Col>
+                            </Row>
+                        </LightSpeed>
+                    </Carousel.Item>
+                </Carousel>
+            </article>
         </Container>
     );
 };
